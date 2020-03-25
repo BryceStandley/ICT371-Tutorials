6c3ab566d5a8e68727d829747cc2a21aa96b2e36
Bryce Standley <bryce@vectorpixel.net>
2020-03-25 13:44:25 +0800

    Updated Tut 2

---
+++ Tutorial 2/ICT371 Tutorial 2/Assets/Scripts/CameraLookAt.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLookAt : MonoBehaviour
+{
+    //Put this on a camera that moves around the scene
+
+    public Transform target; // Object you want to follow 
+
+    private void Update()
+    {
+        transform.LookAt(target); //Looks at the object every frame
+    }
+}
---
+++ Tutorial 2/ICT371 Tutorial 2/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public float distanceToMove = 8f;
+
+    private void Update()
+    {
+        transform.position = new Vector3(Mathf.PingPong(Time.time, distanceToMove), transform.position.y, transform.position.z);
+    }
+}
