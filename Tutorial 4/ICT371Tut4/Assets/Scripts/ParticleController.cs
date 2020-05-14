9e3bc8144bc1a9c60482c933bacf9d2afc30cbd1
Bryce Standley <bryce@vectorpixel.net>
2020-05-14 09:25:05 +0800

    tut4.5

---
+++ Tutorial 4/ICT371Tut4/Assets/Scripts/ParticleController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour
+{
+    public ParticleSystem unityParticeSystem;
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            unityParticeSystem.Play();
+        }
+    }
+}
