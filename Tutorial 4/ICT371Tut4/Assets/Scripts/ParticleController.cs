3a93e95598a1c7ec810700d252565946ac053217
Bryce Standley <bryce@vectorpixel.net>
2020-05-14 10:00:31 +0800

    tut4.6

--- Tutorial 4/ICT371Tut4/Assets/Scripts/ParticleController.cs
+++ Tutorial 4/ICT371Tut4/Assets/Scripts/ParticleController.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class ParticleController : MonoBehaviour
 {
     public ParticleSystem unityParticeSystem;
+    public VisualEffect vfxGraph;
+
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.Alpha1))
         {
             unityParticeSystem.Play();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            vfxGraph.Play();
         }
     }
 }
