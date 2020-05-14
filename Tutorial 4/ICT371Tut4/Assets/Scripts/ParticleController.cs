902ffa2a4feac7f5d58418b01f8aafde051ea0a5
Bryce Standley <bryce@vectorpixel.net>
2020-05-14 12:00:30 +0800

    tut4.7
    finished particles

--- Tutorial 4/ICT371Tut4/Assets/Scripts/ParticleController.cs
+++ Tutorial 4/ICT371Tut4/Assets/Scripts/ParticleController.cs
@@ -8,16 +8,30 @@ public class ParticleController : MonoBehaviour
     public ParticleSystem unityParticeSystem;
     public VisualEffect vfxGraph;
 
+    public GameObject particleJobSystem;
+
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.Alpha1))
         {
+            vfxGraph.Stop();
+            particleJobSystem.SetActive(false);
             unityParticeSystem.Play();
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
+            unityParticeSystem.Stop();
+            particleJobSystem.SetActive(false);
             vfxGraph.Play();
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            unityParticeSystem.Stop();
+            vfxGraph.Stop();
+            particleJobSystem.SetActive(true);
+            particleJobSystem.GetComponent<ParticleJob>().m_ParticleSystem.Play();
         }
     }
 }
---
+++ Tutorial 4/ICT371Tut4/Assets/Scripts/ParticleJob.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.ParticleSystemJobs;
+ [RequireComponent(typeof(ParticleSystem))]
+public class ParticleJob : MonoBehaviour
+{
+    public ParticleSystem m_ParticleSystem;
+    UpdateParticlesJob m_Job = new UpdateParticlesJob();
+ 
+    private void Awake()
+    {
+        m_ParticleSystem = GetComponent<ParticleSystem>();
+        MainStart();
+    }
+    public void MainStart()
+    {
+        Debug.Log("test");
+        
+        m_ParticleSystem.Simulate(2.0f);
+        m_ParticleSystem.Play();
+        
+ 
+        var main = m_ParticleSystem.main;
+        main.simulationSpeed = 0.0f;
+        this.gameObject.SetActive(false);
+    }
+    void OnParticleUpdateJobScheduled()
+    {
+        m_Job.m_Time = Time.time;
+        m_Job.Schedule(m_ParticleSystem);
+    }
+ 
+    //[BurstCompile] // Enable if using the Burst package
+    struct UpdateParticlesJob : IJobParticleSystem
+    {
+        public float m_Time;
+ 
+        public void Execute(ParticleSystemJobData particles)
+        {
+            var sizes = particles.sizes.x;
+ 
+            for (int i = 0; i < particles.count; i++)
+                sizes[i] = Mathf.Sin(m_Time) + 1.0f;
+        }
+    }
+}
